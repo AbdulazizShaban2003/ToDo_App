8aa70a1a9b68e88f5db238e36a032afa829db931
abdulaziz <abdulazizshaban9860@gmail.comgit config --global user.email abdulazizshaban9860@gmail.com>
2025-03-15 02:21:45 +0200

    finish app and local notification

--- lib/core/services/di.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get_it/get_it.dart';
-
-import '../database/cache_helper.dart';
-
-final  s1 =GetIt.instance;
-
-void setup() {
-  s1.registerSingleton<CacheHelper>(CacheHelper());
-}
